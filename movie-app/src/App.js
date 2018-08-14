e191bc1c9c317f4bfb130185b7e21615f152b0b3
lionem2018 <lionem2018@gmail.com>
2018-08-15 02:37:57 +0900

    간단한 Loading 화면 적용하기(state의 정보 유무 확인)

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -5,25 +5,6 @@ import Movie from './Movie';
 class App extends Component {
 
   state = {
-    greeting: 'Hello!',
-    movies: [
-      {
-        title: "Matrix",
-        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
-      },
-      {
-        title: "Full Metal Jacket",
-        poster: "https://ia.media-imdb.com/images/M/MV5BNzc2ZThkOGItZGY5YS00MDYwLTkyOTAtNDRmZWIwMGRhYTc0L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-      },
-      {
-        title: "Oldboy",
-        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-      },
-      {
-        title: "Star Wars",
-        poster: "https://ia.media-imdb.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg"
-      }
-    ]
     
   }
 
@@ -31,15 +12,38 @@ class App extends Component {
     setTimeout(() => {
       this.setState({
         movies: [
-          //기존 영화 정보들을 그대로 두고, 새로운 영화 정보 추가
+          {
+            title: "Matrix",
+            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
+          },
+          {
+            title: "Full Metal Jacket",
+            poster: "https://ia.media-imdb.com/images/M/MV5BNzc2ZThkOGItZGY5YS00MDYwLTkyOTAtNDRmZWIwMGRhYTc0L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+          },
+          {
+            title: "Oldboy",
+            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+          },
+          {
+            title: "Star Wars",
+            poster: "https://ia.media-imdb.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg"
+          },
           {
             title: 'Trainspotting',
             poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/71/Trainspotting_ver2.jpg/250px-Trainspotting_ver2.jpg'
-          },
-          ...this.state.movies  //기존 영화 정보들을 그대로 둠
+          }
         ]
       })
     },5000)
+  }
+
+  //리액트 자체 기능이 많기 때문에 사용자 정의 함수는 구별을 위해 시작시 _(언더바) 사용
+  _renderMoives = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    })
+
+    return movies
   }
 
   render() {
@@ -47,9 +51,9 @@ class App extends Component {
     // 데이터 관련 작업
     return (
       <div className="App">
-       {this.state.movies.map((movie, index) => {
-         return <Movie title={movie.title} poster={movie.poster} key={index}/>
-       })}
+        { // this.state에 movies라는 정보가 존재하는지 여부를 확인한 후,
+          // 경우에 따라 _renderMovies()함수가 실행되거나, Loading 문구가 출력되도록 함
+          this.state.movies? this._renderMoives() : 'Loading'}
       </div>
     );
   }
