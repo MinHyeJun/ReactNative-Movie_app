18ddd7da1ccf63ff07870f69c84ad599c0e9f022
lionem2018 <lionem2018@gmail.com>
2018-08-16 00:15:42 +0900

    리액트에서 AJAX 사용하기(영화 정보 가져오기)

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -4,37 +4,10 @@ import Movie from './Movie';
 
 class App extends Component {
 
-  state = {
-    
-  }
+  state = { }
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
-          },
-          {
-            title: "Full Metal Jacket",
-            poster: "https://ia.media-imdb.com/images/M/MV5BNzc2ZThkOGItZGY5YS00MDYwLTkyOTAtNDRmZWIwMGRhYTc0L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-          },
-          {
-            title: "Oldboy",
-            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-          },
-          {
-            title: "Star Wars",
-            poster: "https://ia.media-imdb.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg"
-          },
-          {
-            title: 'Trainspotting',
-            poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/71/Trainspotting_ver2.jpg/250px-Trainspotting_ver2.jpg'
-          }
-        ]
-      })
-    },5000)
+    console.log(fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating'))
   }
 
   //리액트 자체 기능이 많기 때문에 사용자 정의 함수는 구별을 위해 시작시 _(언더바) 사용
