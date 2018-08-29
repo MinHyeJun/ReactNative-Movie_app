f255f9d84924267fcffdab7316e0e49dbc826f8c
lionem2018 <lionem2018@gmail.com>
2018-08-30 01:49:21 +0900

    flexbox 활용해서 css 수정해보기 (레이아웃 꾸며보기)

--- fucking-weather/App.js
+++ fucking-weather/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello! I'm a Native Developer Now! And I loooooove REACT!!</Text>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
       </View>
     );
   }
@@ -15,7 +30,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+    flexWrap: 'nowrap'
   },
+  redView: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'red',
+  },
+  yellowView: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'yellow',
+  }
 });
