3d8993f8bae81708362af9bb365f1f3da738288b
lionem2018 <lionem2018@gmail.com>
2018-08-31 00:52:04 +0900

    OpenWeatherMap API를 통해 현재 위치의 날씨 정보 받기

--- fucking-weather/App.js
+++ fucking-weather/App.js
@@ -2,25 +2,39 @@ import React, { Component }from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Weather from './Weather';
 
+const API_KEY = "fb43216c5c20091d3757721bcf32229e";
+
 export default class App extends React.Component {
   state = {
     isLoaded: false,
-    error: null
+    error: null,
+    temperature: null,
+    name: null
   };
 
   componentDidMount(){
     navigator.geolocation.getCurrentPosition(
       position =>{
-        this.setState({
-          isLoaded: true
-        });
+        this._getWeather(position.coords.latitude, position.coords.longitude);
       },
       error => {
         this.setState({
           error: error
-        })
+        });
       }
     );
+  }
+
+  _getWeather = (lat, lon) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      this.setState({
+        temperature: json.main.temp,
+        name: json.weather[0].main,
+        isLoaded: true
+      });
+    })
   }
 
   render() {
